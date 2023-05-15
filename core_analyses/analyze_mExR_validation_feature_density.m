9bcd40f6be39cbe3d06d2a4d08a557629e66944d
Margaret Schroeder <mschro@mit.edu>
2023-05-15 11:31:49 -0400

    Update analyze_mExR_validation_feature_density.m
    added flexibility on arrangement of reference channel (whether to sum across all channels or not), and some other tweaks

--- core_analyses/analyze_mExR_validation_feature_density.m
+++ core_analyses/analyze_mExR_validation_feature_density.m
@@ -8,44 +8,101 @@ nchannels = params.nchannels;
 parentfolder = params.parentfolder;
 nrounds = params.nrounds;
 
-for rridx = 1:nrounds %loop through each round
-    for chidx = 1:nchannels %loop through each channel
+if params.regsummed
+    for rridx = 1:nrounds %loop through each round
+        for chidx = 1:nchannels %loop through each channel
+                
+            fnames = dir([parentfolder '*' fov '*' 'round*' num2str(rridx) '*ch0' num2str(chidx) '*.tif']); 
+            img = loadtiff([parentfolder fnames(1).name]);
             
-        fnames = dir([parentfolder '*' fov '*' 'round*' num2str(rridx) '*ch0' num2str(chidx) '*.tif']); 
-        img = loadtiff([parentfolder fnames(1).name]);
-        
-        if strcmp(params.normalization,'minmax')
-            img = mat2gray(img);
-        end
-        
-        imbin = binarize_intensity_threshold(img,params);
+            if chidx == 1
+                masksummed = zeros(size(img));
+            end
 
-        closingrad = params.closingrad*(1/xystep); %radius for morphological closing, in nm
-        if params.morph_close
-                %I_c=imclose(I_f0,strel('sphere',closingrad)); 
-            imclosed=imclose(imbin,strel('disk',floor(closingrad))); %morphological closing
-        else
-            imclosed=imbin; %no morphological closing
+            if strcmp(params.normalization,'minmax')
+                img = mat2gray(img);
+            end
+            
+            imbin = binarize_intensity_threshold(img,params);
+    
+            closingrad = params.closingrad*(1/xystep); %radius for morphological closing, in nm
+            if params.morph_close
+                    %I_c=imclose(I_f0,strel('sphere',closingrad)); 
+                imclosed=imclose(imbin,strel('disk',floor(closingrad))); %morphological closing
+            else
+                imclosed=imbin; %no morphological closing
+            end
+            
+            %apply lower filter on both after closing
+            %set lower limit on ROI size to minimize noise: .1 um in each dimension
+            lowerlim = ceil((params.lowerlim^3)*(1/xystep)*(1/xystep)*(1/zstep));
+            mask = bwareaopen(imclosed, lowerlim); %filtration, removes binary objects LESS than this size
+            masksummed = or(mask,masksummed);
         end
-        
-        %apply lower filter on both after closing
-        %set lower limit on ROI size to minimize noise: .1 um in each dimension
-        lowerlim = ceil((params.lowerlim^3)*(1/xystep)*(1/xystep)*(1/zstep));
-        mask = bwareaopen(imclosed, lowerlim); %filtration, removes binary objects LESS than this size
 
-        CC = bwconncomp(mask,26);%find all connected components in the filtered
+        CC = bwconncomp(masksummed,26);%find all connected components in the filtered
         nobjects = CC.NumObjects;
-
+    
         cc_volumes = regionprops3(CC,'Volume');
         cc_volumes=cc_volumes.Volume;
         
-        nobjects(chidx,rridx) = nobjects;
-        volume_temp = nnz(mask(:));
-        volume(chidx,rridx) = volume_temp * params.vol_converter;
-        volume_frac(chidx,rridx) = volume_temp/numel(mask);
-        min_vol(chidx,rridx) = min(cc_volumes)/volume_temp;
-        max_vol(chidx,rridx) = max(cc_volumes)/volume_temp;
+        nobjects(rridx) = nobjects;
+        volume_temp = nnz(masksummed(:));
+        volume(rridx) = volume_temp * params.vol_converter;
+        volume_frac(rridx) = volume_temp/numel(masksummed);
+        min_vol(rridx) = min(cc_volumes)/volume_temp;
+        max_vol(rridx) = max(cc_volumes)/volume_temp;
 
+        figure()
+        imagesc(max(masksummed,[],3))
+        colormap gray
+
+        if params.writemask %save down the mask
+            filename = [params.savefolder fov '_' 'round00' num2str(rridx) '_summedMask_MIP.tif'];
+            imwrite(uint8(max(masksummed,[],3)),filename)
+        end
+    
+    end
+else
+    for rridx = 1:nrounds %loop through each round
+        for chidx = 1:nchannels %loop through each channel
+                
+            fnames = dir([parentfolder '*' fov '*' 'round*' num2str(rridx) '*ch0' num2str(chidx) '*.tif']); 
+            img = loadtiff([parentfolder fnames(1).name]);
+            
+            if strcmp(params.normalization,'minmax')
+                img = mat2gray(img);
+            end
+            
+            imbin = binarize_intensity_threshold(img,params);
+    
+            closingrad = params.closingrad*(1/xystep); %radius for morphological closing, in nm
+            if params.morph_close
+                    %I_c=imclose(I_f0,strel('sphere',closingrad)); 
+                imclosed=imclose(imbin,strel('disk',floor(closingrad))); %morphological closing
+            else
+                imclosed=imbin; %no morphological closing
+            end
+            
+            %apply lower filter on both after closing
+            %set lower limit on ROI size to minimize noise: .1 um in each dimension
+            lowerlim = ceil((params.lowerlim^3)*(1/xystep)*(1/xystep)*(1/zstep));
+            mask = bwareaopen(imclosed, lowerlim); %filtration, removes binary objects LESS than this size
+    
+            CC = bwconncomp(mask,26);%find all connected components in the filtered
+            nobjects = CC.NumObjects;
+    
+            cc_volumes = regionprops3(CC,'Volume');
+            cc_volumes=cc_volumes.Volume;
+            
+            nobjects(chidx,rridx) = nobjects;
+            volume_temp = nnz(mask(:));
+            volume(chidx,rridx) = volume_temp * params.vol_converter;
+            volume_frac(chidx,rridx) = volume_temp/numel(mask);
+            min_vol(chidx,rridx) = min(cc_volumes)/volume_temp;
+            max_vol(chidx,rridx) = max(cc_volumes)/volume_temp;
+    
+        end
     end
 end
 
