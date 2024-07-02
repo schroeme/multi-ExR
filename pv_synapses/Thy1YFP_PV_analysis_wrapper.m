0ddd1cdc6ef36bd34d4759460ac075788f7f040c
Margaret Schroeder <65419570+schroeme@users.noreply.github.com>
2024-07-01 20:51:39 -0400

    Initialize PV synapses multiExR code base

---
+++ pv_synapses/Thy1YFP_PV_analysis_wrapper.m
@@ -0,0 +1,149 @@
+%%  WRAPPER for analyzing Thy1-YFP vs. PV synapses %% 
+
+% Last modified by Margaret Schroeder on 6/8/24
+
+% Image processing up to this point:
+% - background subtraction in Fiji and
+% - registration using modified ExSeq pipeline
+% - manual ROI identification (for some sections) and running the Fiji
+% script to automatically crop these
+
+% NOTE: It's best to run this wrapper script section by section and be
+% mindful of the outputs
+
+%% Set up and parameter setting
+
+clear all
+
+%the directory containing all of your image files, named according to the
+%following convention: [FIELD OF VIEW NAME]_[ROUND]_[CHANNEL].tif, as
+%output from the ExSeqProcessing registration pipeline
+params.parentfolder = 'A:/Margaret/mExR/2024.04_Thy1-YFP_PV/cropped_synaptic_rois/PN/';
+
+%names of fields of view within the parent folder
+ROIs = {
+     'Sample1-CA1-fov1';
+    'Sample1-CA1-fov2';
+    'Sample1-CA1-fov3';
+    'Sample1-CA1-fov4';
+    'Sample1-CA1-fov5';
+    %'Sample1-CA1-fov6'; poor registration
+    'Sample1-CA1-fov7';
+    'Sample1-CA1-fov8';
+    'Sample1-CA1-fov9';
+    'Sample1-Ctx-fov1';
+    'Sample1-Ctx-fov2';
+    'Sample1-Ctx-fov3';
+    'Sample1-Ctx-fov4';
+    'Sample1-Ctx-fov5';
+   % 'Sample1-Ctx-fov6'; poor registration
+   % 'Sample2-CA1-fov1'; poor registration
+   % 'Sample2-CA1-fov2'; poor registration
+   % 'Sample2-CA1-fov4'; poor registration
+    'Sample2-CA1-fov5';
+    'Sample2-CA1-fov7';
+    'Sample2-CA1-fov8';
+    'Sample2-CA1-fov9';
+    'Sample2-Ctx-fov1';
+    'Sample2-Ctx-fov2';
+    'Sample2-Ctx-fov3';
+    'Sample2-Ctx-fov4';
+    'Sample2-Ctx-fov5';
+    'Sample2-Ctx-fov6';
+    'Sample2-Ctx-fov7';
+    'Sample2-Ctx-fov8';
+    'Sample2-Ctx-fov9'
+    };
+
+
+%set parameters
+params.xystep = 0.1625/18; %physical pixel size divided by expansoin factor, um/voxel in x and y
+params.zstep = 0.25/18; %physical z-step size divided by expansion factor, um/voxel in z
+% params.thresh_method = 'pct'; %method for intensity thresholding; either percentile, z-score based, or absolute
+% params.thresh_pctl = 99; %percentile at which to set intensity threshold
+params.dofilt = 1; %1 to do median filter to eliminate high spatial frequency noise
+params.filt_size = [5 5 3]; %filter size [x y z] for 3D median filter
+params.filt = 'med'; %method for filtering image
+
+% params.doplot = 0; %whether or not to plot raw and thresholded images for inspection
+% params.morph_ch = '1'; %reference/morphology channel
+% params.morph_round = '1'; %reference/morphology round
+% params.subtract_morph=1; %whether or not to subtract out the morphology/reference channel prior to quantifying other signal in the field of view
+% params.lowerlim = 100; %minimum object size, in voxels
+% params.upperlim = 5000; %maximum object size, in voxels
+% 
+% params.syn_rad=10; %radius for morphological closing (dilation) of synapses
+% params.morph_close_syn = 1; %whether or not to morphologically close synapses
+params.vol_converter = ((2/3)*params.xystep + (1/3)*params.zstep)^3; %weighted average to convert voxels to um^3
+
+%define synaptic channels using 'round-channel'
+params.syn_channels = {
+    '01-1';%Cav2.1
+   '01-2';%PV
+   '01-3';%GFP
+    '02-2';%GluA4
+    '02-3';%NR1
+    '03-2';%GluA1
+    '03-3';%PSD95
+    '04-2';%Stg
+    '04-3';%NR2B
+    '05-2';%PSD95
+    '05-3';%Shank3
+    '06-2';%Btbd11
+    '06-3';%IRSp53
+    };
+
+
+%% Run quantification of synaptic proteins in synaptic ROIs - PN (pyramidal neuron)
+
+%change the parameters for this analysis
+params.doplot=0;
+params.sizefilt=10;
+params.lowerlim=0;
+params.thresh_method = 'zscore';
+params.thresh_multiplier = 3;
+%params.filt=0;
+
+for fidx = 1:length(ROIs)
+    data(fidx).roiname = ROIs{fidx};
+    [data(fidx).data] = analyze_synaptic_puncta_cropped(params,ROIs{fidx},params.syn_channels);
+end
+
+%% Compile data - synaptic proteins in PNs
+%This section puts the data in a convenient format for copy/paste into
+%Excel or Prism for graphing. May require hard-coding to your preferences
+topaste = [];
+for ii = 1:length(ROIs)
+    mat = cell2mat(data(ii).data.AR); %change this to pull different variables of interest
+    topaste = [topaste; mat];
+end
+
+
+%% Run quantification of synaptic proteins in cropped abeta nanoclusters - PV (pyramidal neuron)
+
+params.parentfolder = 'A:/Margaret/mExR/2024.04_Thy1-YFP_PV/cropped_synaptic_rois/PV/';
+
+%change the parameters for this analysis
+params.doplot=0;
+params.sizefilt=10;
+params.lowerlim=0;
+params.thresh_method = 'zscore';
+params.thresh_multiplier = 3;
+%params.filt=0;
+
+for fidx = 1:length(ROIs)
+    data(fidx).roiname = ROIs{fidx};
+    [data(fidx).data] = analyze_synaptic_puncta_cropped(params,ROIs{fidx},params.syn_channels);
+end
+
+%% Compile data - synaptic proteins in pvs
+%This section puts the data in a convenient format for copy/paste into
+%Excel or Prism for graphing. May require hard-coding to your preferences
+topaste = [];
+for ii = 1:length(ROIs)
+    mat = cell2mat(data(ii).data.meanint); %change this to pull different variables of interest
+    %mat = data(ii).data.roi_filename;
+    topaste = [topaste; mat];
+    %topaste = [topaste; mat{1,1}];
+end
+
---
+++ pv_synapses/crop_ROIs_zrestrict.ijm
@@ -0,0 +1,113 @@
+// ************* FIJI SCRIPT FOR AUTOMATICALLY CROPPING MANUALLY-IDENTIFIED ROIS ************* \\
+// Last modified by MES on 6/8/24
+// Please read through this script and take note of anything marged "CHANGE HERE". Change accordingly
+// Lots of this script is hard-coded and will need to be modified based on your file saving/naming conventions
+// Some familiarity with Fiji / ijm is best
+// I have tried to compensate for this with good commenting
+
+//CHANGE ALL OF THE BELOW AS NEEDED
+parent = "A:/Margaret/mExR/2024.04_Thy1-YFP_PV/registered/Round1_noSMI_noGFAP/"; //where whole field of view images are stored
+target = "A:/Margaret/mExR/2024.04_Thy1-YFP_PV/cropped_synaptic_rois/"; //where to save cropped images
+roiparent = "A:/Margaret/mExR/2024.04_Thy1-YFP_PV/ROIs/"; //where the .zip files for Fiji ROIs are saved
+
+roitypes = newArray("PV","PN");
+
+ROIs = newArray( //fields of view to process
+"Sample1-CA1-fov1",
+"Sample1-CA1-fov2",
+"Sample1-CA1-fov3",
+"Sample1-CA1-fov4",
+"Sample1-CA1-fov5",
+"Sample1-CA1-fov6",
+"Sample1-CA1-fov7",
+"Sample1-CA1-fov8",
+"Sample1-CA1-fov9",
+"Sample1-Ctx-fov1",
+"Sample1-Ctx-fov2",
+"Sample1-Ctx-fov3",
+"Sample1-Ctx-fov4",
+"Sample1-Ctx-fov5",
+"Sample1-Ctx-fov6",
+"Sample2-CA1-fov1",
+"Sample2-CA1-fov2",
+////"Sample2-CA1-fov3", exclude this one!
+"Sample2-CA1-fov4",
+"Sample2-CA1-fov5",
+"Sample2-CA1-fov7",
+"Sample2-CA1-fov8",
+"Sample2-CA1-fov9",
+"Sample2-Ctx-fov1",
+"Sample2-Ctx-fov2",
+"Sample2-Ctx-fov3",
+"Sample2-Ctx-fov4",
+"Sample2-Ctx-fov5",
+"Sample2-Ctx-fov6",
+"Sample2-Ctx-fov7",
+"Sample2-Ctx-fov8",
+"Sample2-Ctx-fov9"
+				);
+				
+nslices_range = 15; //number of z-slices in each direction from ROI midpoint to crop. total z-stack length for each ROI will be double this
+
+// NO NEED TO MODIFY THE BELOW
+image_folder = parent;
+list = getFileList(image_folder);									 //find all files in current data folder					
+extension = "tif";	
+run("Close All");
+for  (i=0; i<ROIs.length; i++){
+	ROI = ROIs[i];
+	for (m=0; m<roitypes.length; m++){
+		roitype = roitypes[m];
+		targetdir = target + roitype + "/";
+		
+		for (j=0; j<list.length; j++) {		//loop through all images in the image folder
+			if (endsWith(toLowerCase(list[j]), "." + extension)) {
+				if (indexOf(list[j], ROI) >= 0){
+					name =  list[j];
+			    	aIndex = indexOf(name, ".tif");										//record position of dot that separates extension from filename	
+			    	title = substring(name, 0, aIndex); //record the abbreviated file name (excluding file extension; eg:'stack1')
+					
+					roidir = roiparent + roitype +"/";
+				    roiManager("Open", roidir + ROI + "_" + roitype + ".zip"); //open the manually-identified ROIs. NOTE: they must be named the same as the prefix for the .tif stacks
+				    open(parent + name);
+				    getDimensions(width, height, channels, slices, frames);
+				    
+					// Iterate all ROIs in ROI Manager
+				    for (n=0; n<roiManager("count"); ++n) {
+				    	roiManager("Select", n);
+				    	zidx = getSliceNumber();
+				    	zrange1 = zidx-nslices_range;
+				    	zrange2 = zidx+nslices_range;
+	
+				    	if (zrange1<1){
+				    		zrange1=1;
+				    	}
+				    	if (zrange2>slices){
+				    		zrange=slices;
+				    	}
+	
+				    	getSelectionBounds(x,y,width,height);
+				    	
+				    	run("Duplicate...", "duplicate range=" + toString(zrange1) + "-" + toString(zrange2));
+				        //saveAs("Tiff", target + title + "_ROI_"+toString(x)+"_"+toString(y)+".tif");
+				        if (n+1 < 10){
+				        	roino = "0" + toString(n+1);
+				        } else {
+				        	roino = toString(n+1);
+				        }
+				        title =  replace(title, "round002", "round001");//NOTE: replaced round001 with GFAP/SMI with round001 WITHOUT GFAP/SMI (has Cav2.1 alone)
+				        saveAs("Tiff", targetdir + title + "_" + roitype + "_ROI"+roino+".tif");
+				        close();
+				    }
+				    selectWindow(name);
+				    run("Close");
+				    roiManager("Delete");
+				    selectWindow("ROI Manager");
+				    run("Close");
+				}
+			}
+		}
+	}
+}
+run("Close All");
+run("Collect Garbage");
